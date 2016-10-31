1781a635bd0cdf93bc51aaed1cd97a5209dc1467
Konrad Wojas <github@m.wojas.nl>
2016-10-31 15:21:20 +0800

    Initial commit

---
+++ helpers/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+dest=${dest:-docker.ovpn}
+
+if [ ! -f "/local/$dest" ]; then
+    echo "*** REGENERATING ALL CONFIGS ***"
+    set -ex
+    #rm -rf /etc/openvpn/*
+    ovpn_genconfig -u tcp://localhost
+    sed -i 's|^push|#push|' /etc/openvpn/openvpn.conf
+    echo localhost | ovpn_initpki nopass
+    easyrsa build-client-full host nopass
+    ovpn_getclient host | sed '
+    	s|localhost 1194|localhost 13194|;
+	s|redirect-gateway.*|route 172.16.0.0 255.240.0.0|;
+    ' > "/local/$dest"
+fi
+
+exec ovpn_run
