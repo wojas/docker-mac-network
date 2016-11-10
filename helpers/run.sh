06f596fba4669cd6a9c9ef9d463bba97494f7c8d
Tobias Käfer <tobiassebastian.kaefer@1und1.de>
2016-11-10 11:45:50 +0100

    Make proxy forwarding port configurable

--- helpers/run.sh
+++ helpers/run.sh
@@ -3,6 +3,7 @@
 dest=${dest:-docker.ovpn}
 network=${DOCKER_NETWORK:-172.16.0.0}
 netmask=${DOCKER_NETMASK:-255.240.0.0}
+forward_port=${FORWARD_PORT:-13194}
 
 if [ ! -f "/local/$dest" ]; then
     echo "*** REGENERATING ALL CONFIGS ***"
@@ -12,7 +13,7 @@ if [ ! -f "/local/$dest" ]; then
     sed -i 's|^push|#push|' /etc/openvpn/openvpn.conf
     echo localhost | ovpn_initpki nopass
     easyrsa build-client-full host nopass
-    ovpn_getclient host | sed "s|localhost 1194|localhost 13194|;s|redirect-gateway.*|route ${network} ${netmask}|;" > "/tmp/out/$dest"
+    ovpn_getclient host | sed "s|localhost 1194|localhost ${forward_port}|;s|redirect-gateway.*|route ${network} ${netmask}|;" > "/tmp/out/$dest"
 fi
 
 exec ovpn_run
